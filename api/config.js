e8ec47056322a4a9ab811745607979e653da5547
Clay Diffrient <cdiffrient@instructure.com>
2016-09-19 22:11:41 -0600

    Fix things to work deployed to simplydiffrient

--- api/config.js
+++ api/config.js
@@ -1,3 +1,14 @@
-exports.host = process.env.RETHINKDB_HOST || 'rethinkdb';
-exports.port = process.env.RETHINKDB_PORT || 28015;
-exports.db = process.env.RETHINKDB_DB || 'attendance';
+let host = 'rethinkdb';
+let port = 28015;
+let db = 'attendance';
+
+if (process.env.RETHINKDB_URL) {
+  const urlTester = /\/\/(.*):(\d{5})/;
+  const matches = process.env.RETHINKDB_URL.match(urlTester);
+  host = matches[1];
+  port = matches[2];
+}
+
+exports.host = process.env.RETHINKDB_HOST || host;
+exports.port = process.env.RETHINKDB_PORT || port;
+exports.db = process.env.RETHINKDB_DB || db;
