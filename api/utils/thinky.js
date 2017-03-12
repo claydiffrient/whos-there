64ee7b9fa3e18d5fedbfc91880c25d3fc55ea7c2
Clay Diffrient <clay.diffrient@gmail.com>
2017-03-11 22:14:17 -0700

    Remove everything for a fresh start

--- api/utils/thinky.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config');
-const thinky = require('thinky')({
-  host: config.host,
-  port: config.port,
-  db: config.db
-});
-
-module.exports = thinky;
--- app/web/index.js
+++
@@ -1,5 +0,0 @@
-import App from './app/App';
-import ReactDOM from 'react-dom';
-import React from 'react';
-
-ReactDOM.render(<App />, document.getElementById('main'));
